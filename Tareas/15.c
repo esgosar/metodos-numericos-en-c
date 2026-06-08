4297f8513666569416b184ba1cc062a3edfe95c4
Estuardo <3354094580901@ingenieria.usac.edu.gt>
2026-06-08 01:06:43 -0600

    Ejercicios del #11 al #15

--- Tareas/15.c
+++ Tareas/15.c
@@ -4,16 +4,16 @@
 #include "../metodos/solvers.h"
 
 double f13(double x) { 
-    return (x * x) - 3.0; 
+    return (x * x * x) - 25.0; 
 }
 
 int main() {
     printf("==============================================================\n");
-    printf("   EJERCICIO 13: Aproximacion de raiz cuadrada de 3\n");
+    printf("   EJERCICIO 13: Aproximacion de raiz cubica de 3\n");
     printf("==============================================================\n");
 
-    printf("\nEvaluando en [1, 2] con Tol: 10^-4\n");
-    biseccion(f13, 1.0, 2.0, 1e-4, 100, 6);
+    printf("\nEvaluando en [2,3] con Tol: 10^-4\n");
+    biseccion(f13, 2.0, 3.0, 1e-4, 100, 6);
 
     return 0;
 }
